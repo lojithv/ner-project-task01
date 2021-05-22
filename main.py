9e23af333d38e8503514ddcc074bad1353dc6479
lkmSasanga <lkmsasanga@gmail.com>
2021-05-22 12:11:31 +0530

    function added and train spacy step 2

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # Load a spacy model and check if it has ner
 import spacy
+import random
+from spacy.util import minibatch, compounding
+from pathlib import Path
 
 nlp = spacy.load('es_core_news_md')
 
 # Getting the pipeline component
-ner=nlp.get_pipe("ner")
+ner = nlp.get_pipe("ner")
 
 text = "Una mujer de 27 años sin antecedentes, residente del hospital, " \
        "presentó odinofagia seguida de artralgia difusa y una erupción de placas eritematosas " \
@@ -21,8 +24,15 @@ doc = nlp(text)
 for ent in doc.ents:
     print(ent.text, ent.label_)
 
-# training data
-TRAIN_DATA = [
+
+################### Train Spacy NER.###########
+
+def train_spacy():
+  # Getting the pipeline component
+  # ner = nlp.get_pipe("ner")
+
+  # training data
+  TRAIN_DATA = [
               ("Una mujer de 27 años sin antecedentes, residente del hospital, presentó odinofagia seguida de"
                " artralgia difusa y una erupción de placas eritematosas pruriginosas extendidas, con una afectación "
                "básicamente facial y acra. El diagnóstico de urticaria fue confirmado por un dermatólogo.",
@@ -30,14 +40,42 @@ TRAIN_DATA = [
               ]
 
 
-# Adding labels to the `ner`
-for _, annotations in TRAIN_DATA:
-  for ent in annotations.get("entities"):
-    ner.add_label(ent[2])
+  # Adding labels to the `ner`
+  for _, annotations in TRAIN_DATA:
+    for ent in annotations.get("entities"):
+      print('Adding labels to the ner...')
+      ner.add_label(ent[2])
 
-# Disable pipeline components you dont need to change
-pipe_exceptions = ["ner", "trf_wordpiecer", "trf_tok2vec"]
-unaffected_pipes = [pipe for pipe in nlp.pipe_names if pipe not in pipe_exceptions]
+  # Disable pipeline components you dont need to change
+  pipe_exceptions = ["ner", "trf_wordpiecer", "trf_tok2vec"]
+  unaffected_pipes = [pipe for pipe in nlp.pipe_names if pipe not in pipe_exceptions]
+
+
+  # TRAINING THE MODEL
+  with nlp.disable_pipes(*unaffected_pipes):
+
+    # Training for 30 iterations
+    for iteration in range(30):
+      print('Iteration started... ' + str(iteration))
+
+      # shuufling examples  before every iteration
+      random.shuffle(TRAIN_DATA)
+      losses = {}
+      # batch up the examples using spaCy's minibatch
+      batches = minibatch(TRAIN_DATA, size=compounding(4.0, 32.0, 1.001))
+      for batch in batches:
+          texts, annotations = zip(*batch)
+          nlp.update(
+                      [texts],  # batch of texts
+                      [annotations],  # batch of annotations
+                      drop=0.5,  # dropout - make it harder to memorise data
+                      losses=losses,
+                  )
+          print("Losses", losses)
+
+
+
+
 
 # output doc
 # MISC - Miscellaneous entities, e.g., events
